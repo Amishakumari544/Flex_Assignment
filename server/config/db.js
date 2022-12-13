889bd3aa1a8f6002b9aabbf9160499e4e0fe4722
Aditya <adityas141102@gmail.com>
2022-12-13 18:14:58 +0530

    :bug: bug fixed

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () =>{
     try {
-        const connect = await mongoose.connect(process.env.MONGO_URI);
+        const connect = await mongoose.connect('mongodb+srv://amisha:amisha@cluster0.c2hp3bd.mongodb.net/?retryWrites=true&w=majority');
         console.log(`MONGODB CONNECTED : ${connect.connection.host}`);
     } catch (error) {
         console.log(error);
